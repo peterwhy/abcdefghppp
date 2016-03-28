47fd08dd7d0930fad8245c76d42152046130aee2
Peter Wong <peterwhy@users.noreply.github.com>
2016-03-28 01:12:18 +0100

    Remove Tuple

--- main.hs
+++ main.hs
@@ -2,16 +2,14 @@ module Main where
 
 import Data.List (permutations)
 
-type Tuple = (Int, Int, Int, Int, Int, Int, Int, Int, Int)
-
 numbers :: [Int]
 numbers = 0:[2..9]
 
-toTuple :: [Int] -> Tuple
-toTuple (a:b:c:d:e:f:g:h:_) = (a,b,c,d,e,f,g,h,1)
+addOne :: [Int] -> [Int]
+addOne (a:b:c:d:e:f:g:h:_) = [a,b,c,d,e,f,g,h,1]
 
-isValid :: Tuple -> Bool
-isValid (a,b,c,d,e,f,g,h,p) =
+isValid :: [Int] -> Bool
+isValid (a:b:c:d:e:f:g:h:p:_) =
     and [
         a /= 0,
         c /= 0,
@@ -21,8 +19,8 @@ isValid (a,b,c,d,e,f,g,h,p) =
         10 * a + b - 10 * c - d == 10 * e + f,
         10 * e + f + 10 * g + h == 111 * p]
 
-answers :: [Tuple]
-answers = filter isValid . map toTuple . permutations $ numbers
+answers :: [[Int]]
+answers = filter isValid . map addOne . permutations $ numbers
 
 main :: IO ()
 main = putStrLn . show $ answers
